b030372cb4f6e16da89552d09a64261432ce5794
bimo <shuangdao11ee@outlook.com>
2020-11-23 23:36:42 +0800

    init

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"./gee"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	r.Run(":9999")
+}
+
+//Engine is the uni handler for all requests
+// type Engine struct{}
+
+// func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+// 	switch req.URL.Path {
+// 	case "/":
+// 		fmt.Fprintf(w, "URL.PATH = %q\n", req.URL.Path)
+// 	case "/hello":
+// 		for k, v := range req.Header {
+// 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+// 		}
+// 	default:
+// 		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+// 	}
+// }
+
+// func main() {
+// 	engine := &Engine{}
+// 	//log.Fatal(http.ListenAndServe(":9999", engine))
+// }
+
+// func main() {
+// 	http.HandleFunc("/", indexHandler)
+// 	http.HandleFunc("/hello", helloHandler)
+// 	log.Fatal(http.ListenAndServe(":9999", nil))
+// }
+
+// func indexHandler(w http.ResponseWriter, req *http.Request) {
+// 	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+// }
+
+// func helloHandler(w http.ResponseWriter, req *http.Request) {
+// 	for k, v := range req.Header {
+// 		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+// 	}
+// }
